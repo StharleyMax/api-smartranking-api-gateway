031da2f3a87a4c468ba0ca09111c76702063ceda
Stharley Maxwell <stharleymax@gmail.com>
2022-02-01 13:16:23 -0300

    create event create-category

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,40 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { CreateCategoriesDTO } from './dtos/Categories.dto';
 
 @Controller('api/v1')
 export class AppController {
   private logger = new Logger(AppController.name);
-  constructor() {}
 
-  @Get()
-  getHello(): string {}
+  private clientAdminBackend: ClientProxy;
+
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://guest:guest@127.0.0.1:5672/smartranking'],
+        queue: 'admin-backend',
+      },
+    });
+  }
+
+  @Post('categories')
+  @UsePipes(ValidationPipe)
+  async createCategory(@Body() createCategoryDto: CreateCategoriesDTO) {
+    return await this.clientAdminBackend.emit(
+      'create-category',
+      createCategoryDto,
+    );
+  }
 }
